e0f5ec068258436bd2d2fb3e520bb62e48dfb16f
Andy Fowlston <andy@fabbdev.com>
2023-06-01 15:17:56 +0100

    fix(lb): hide irrelevant pins on filter change
    Fixes #39

--- modules/openy_map_lb/assets/src/js/map.js
+++ modules/openy_map_lb/assets/src/js/map.js
@@ -641,6 +641,9 @@
           }
           return;
         }
+
+        // Hide all the markers prior to showing only those of interest.
+        this.locations.map(loc => loc.marker.setVisible(false));
         var bounds = new google.maps.LatLngBounds();
 
         locations.map((loc) => {
