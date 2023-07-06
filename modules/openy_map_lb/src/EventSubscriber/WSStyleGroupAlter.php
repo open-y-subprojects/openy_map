8ee3428279e8fce8bd17c8c01a2253fcae50e3a5
davyd.burianuvatyi@ua.nestle.com <venia7vedmak@gmail.com>
2023-07-06 10:13:54 +0300

    feat: Create Border style variations

---
+++ modules/openy_map_lb/src/EventSubscriber/WSStyleGroupAlter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\openy_map_lb\EventSubscriber;
+
+use Drupal\y_lb\WSStyleGroupAlterAbstract;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * An event subscriber to alter available WS Style groups for a component.
+ */
+class WSStyleGroupAlter extends WSStyleGroupAlterAbstract implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getAllowedStyleGroups() {
+    return [
+      'inline_block:lb_openy_map' => [
+        'border_style_component',
+      ],
+    ];
+  }
+
+}
